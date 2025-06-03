2a429c64905e335ef034b8fa95c93c5542184306
Nova <mahdi.zrei@gmail.com>
2025-06-03 12:53:12 +0330

    remove useless logs

--- log/observable.go
+++ log/observable.go
@@ -2,6 +2,8 @@ package log
 
 import (
 	"context"
+	"errors"
+	"github.com/sagernet/sing/common/baderror"
 	"io"
 	"os"
 	"time"
@@ -115,8 +117,23 @@ func (l *observableLogger) Log(ctx context.Context, level Level, args []any) {
 		return
 	}
 	nowTime := time.Now()
+	msgStr := F.ToString(args...)
+	if baderror.Contains(errors.New(msgStr),
+		"i/o timeout",
+		"tls: protocol is shutdown",
+		"handle stream request: read request: EOF",
+		"ws closed: 1000",
+		"keepalive timeout",
+		"connection timed out",
+		"read multiplex stream request: EOF",
+		"name error",
+		"connection refused",
+		"drop connections by rule",
+	) {
+		return
+	}
 	if l.needObservable {
-		message, messageSimple := l.formatter.FormatWithSimple(ctx, level, l.tag, F.ToString(args...), nowTime)
+		message, messageSimple := l.formatter.FormatWithSimple(ctx, level, l.tag, msgStr, nowTime)
 		if level == LevelPanic {
 			panic(message)
 		}
@@ -126,7 +143,7 @@ func (l *observableLogger) Log(ctx context.Context, level Level, args []any) {
 		}
 		l.subscriber.Emit(Entry{level, messageSimple})
 	} else {
-		message := l.formatter.Format(ctx, level, l.tag, F.ToString(args...), nowTime)
+		message := l.formatter.Format(ctx, level, l.tag, msgStr, nowTime)
 		if level == LevelPanic {
 			panic(message)
 		}
@@ -136,7 +153,7 @@ func (l *observableLogger) Log(ctx context.Context, level Level, args []any) {
 		}
 	}
 	if l.platformWriter != nil {
-		l.platformWriter.WriteMessage(level, l.platformFormatter.Format(ctx, level, l.tag, F.ToString(args...), nowTime))
+		l.platformWriter.WriteMessage(level, l.platformFormatter.Format(ctx, level, l.tag, msgStr, nowTime))
 	}
 }
 
