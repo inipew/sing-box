792f5411e8dc5d94fbdb7a5d0e09ea173d23ce58
reF1nd <ref1nd@thunder-cloud.cc>
2025-09-07 14:43:12 +0800

    Add `direct_domain_strategy` option to direct outbound

--- option/direct.go
+++ option/direct.go
@@ -16,6 +16,7 @@ type DirectInboundOptions struct {
 
 type _DirectOutboundOptions struct {
 	DialerOptions
+	DirectDomainStrategy DomainStrategy `json:"direct_domain_strategy,omitempty"`
 	// Deprecated: Use Route Action instead
 	OverrideAddress string `json:"override_address,omitempty" schema:"omit"`
 	// Deprecated: Use Route Action instead
