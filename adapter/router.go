7e15cbee79d42a0d6c44c21c06daa11bfdc3ffee
inipew <dhimas@akupew.sbs>
2026-09-05 07:20:16 +0700

    clash-api: Add reload support

--- adapter/router.go
+++ adapter/router.go
@@ -6,7 +6,7 @@ import (
 	"net/netip"
 	"time"
 
-	"github.com/sagernet/sing-tun"
+	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing-tun/gtcpip/header"
 	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
@@ -29,6 +29,8 @@ type Router interface {
 	NeighborResolver() NeighborResolver
 	AppendTracker(tracker ConnectionTracker)
 	ResetNetwork()
+
+	Reload()
 }
 
 type PreMatchAction uint8
